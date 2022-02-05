a670ca29242dacdca62a8d156492049138440dc0
sarievdima88 <sariev_dima@mail.ru>
2022-02-05 20:41:18 +0300

    lesson3

--- lesson3.py
+++ lesson3.py
@@ -49,9 +49,26 @@ b = 4
 c = -12
 result = average(a, b, c, 10)
 print(result)
-"""
+
 z=15
 def local (x):
     y=10
     print('x= {}, y={}, z={}'.format(x,y,z))
 local(5)
+
+"""
+
+
+# позиционные параметры
+def first_func(var_1, var_2, var_3):
+    return var_1 + var_2 + var_3
+
+
+print(first_func(10, 20, 30))
+
+
+# именованные параметры
+def second_func(var_2, var_1, var_3):
+    print(f"Dima - {var_2}; Erdem - {var_1}; Nikolay - {var_3}")
+
+second_func(var_1=10, var_2=20, var_3=30)
