27ac35017bd191aa92a94e7680c7c260bd0132c9
sarievdima88 <sariev_dima@mail.ru>
2022-02-05 22:49:03 +0300

    lesson3

--- lesson3.py
+++ lesson3.py
@@ -88,4 +88,43 @@ def s_calc():
 
 s_side_val, s_circle, s_full_val = s_calc()
 print(f"Площадь боковой пов-ти - {s_side_val};Площадь цилиндра - {s_circle} Полная площадь - {s_full_val}")
+
+def my_func(*args):
+    return
+
+print(my_func(el_1=10, el_2=20, el_3="text"))
+
+my_func = lambda p_1, p_2: p_1 + p_2
+
+print(my_func(2, 5))
+print(my_func("abra", "kadabra"))
+
+print((lambda p_1, p_2: p_1 + p_2)(2, 5))
+print((lambda p_1, p_2: p_1 + p_2)("abra", "kadabra"))
+
+new_func = lambda *args: args
+print(new_func(10, 20, 30, 40))
+
+# f = lambda x: x ** 2
+
+print((lambda x: x / 2)(4))
+
+print(ord("G"))
+print(chr(71))
+print(len("abrakadabra12"))
+
+print(abs(4.0))
+print(round(4,4600))
+print(divmod(9, 2))
+print(pow(16, 2))
+
+iter_obj = [20, 2, 5, 100]
+print(sum(iter_obj))
+iter_obj = (20, 2, 5, 100)
+print(sum(iter_obj))
+iter_obj = {20, 2, 5, 100}
+print(sum(iter_obj))
+
+print(list(range(10)))
+print(tuple(range()))
 """
